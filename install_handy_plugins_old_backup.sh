e5d85440e5b0a3061dcff243e8d550ff5e05f021
Wei Tat Lee <weitat95@live.com>
2019-02-26 14:25:06 +0000

    Use single cat instead of multiples echo

--- install_handy_plugins_old_backup.sh
+++ install_handy_plugins_old_backup.sh
@@ -34,25 +34,21 @@ Plugin 'w0rp/ale'
 call vundle#end()
 filetype plugin indent on
 EOF
-
-    cat >> ~/.vimrc << EOF
-so ~/.vim/plugins.vim
-map <C-o> :NERDTreeToggle<CR>
-set number
-set expandtab
-set shiftwidth=4
-set softtabstop=4
-map <C-l> :set invnumber<cr>
-map <C-t><up> :tabr<cr>
-map <C-t><down> :tabl<cr
-map <C-t><left> :tabp<cr>
-map <C-t><right> :tabn<cr
-let g:ale_enabled = 0 "Disable Ale on default"
-"let g:gitgutter_enabled = 0" "Disable GitGutter on Default"
-map <C-a> :ALEToggle <CR>
-map <C-g> :GitGutterToggle <CR>
-map ; :Files<CR>
-EOF
-
-vim +PluginInstall +qall
+    echo "so ~/.vim/plugins.vim" >> ~/.vimrc
+    vim +PluginInstall +qall
+    echo "map <C-o> :NERDTreeToggle<CR>" >> ~/.vimrc
+    echo "set number" >> ~/.vimrc
+    echo "set expandtab" >> ~/.vimrc
+    echo "set shiftwidth=4" >> ~/.vimrc
+    echo "set softtabstop=4" >> ~/.vimrc
+    echo "map <C-l> :set invnumber<CR>" >> ~/.vimrc
+    echo "map <C-t><up> :tabr<cr>" >> ~/.vimrc
+    echo "map <C-t><down> :tabl<cr>" >> ~/.vimrc
+    echo "map <C-t><left> :tabp<cr>" >> ~/.vimrc
+    echo "map <C-t><right> :tabn<cr>" >> ~/.vimrc
+    echo "let g:ale_enabled = 0 \"Disable Ale on default\"" >> ~/.vimrc
+    echo "\"let g:gitgutter_enabled = 0 \" \"Disable GitGutter on Default\"" >>~/.vimrc
+    echo "map <C-a> :ALEToggle <CR>" >> ~/.vimrc
+    echo "map <C-g> :GitGutterToggle <CR>" >> ~/.vimrc
+    echo "map ; :Files<CR>" >> ~/.vimrc
 fi
