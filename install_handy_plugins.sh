9d9116fa34e0b4c386ec28dbf7bce613980944c7
Wei Tat Lee <weitat95@live.com>
2019-03-04 18:22:26 +0000

    Fix comments next to mapping

--- install_handy_plugins.sh
+++ install_handy_plugins.sh
@@ -38,20 +38,27 @@ fi
 cat >> ~/.vimrc << EOF
 so ~/.vim/plugins.vim
 
-set number "Set Line Number to show by default"
-set expandtab "Tabs as Spaces"
+"Set Line Number to show by default"
+set number 
+"Tabs as Spaces"
+set expandtab 
 set shiftwidth=4 
 set softtabstop=4
 
-map <C-l> :set invnumber<cr> "Ctrl+l to toggle line numebrs"
+"Ctrl+l to toggle line numebrs"
+map <C-l> :set invnumber<cr> 
 
-map <C-t><up> :tabr<cr> "Ctrl+t + arrowKeys to navigate through tabs"
+"Ctrl+t + arrowKeys to navigate through tabs"
+map <C-t><up> :tabr<cr> 
 map <C-t><down> :tabl<cr>
 map <C-t><left> :tabp<cr>
 map <C-t><right> :tabn<cr>
 
-nnoremap <A-j> :m .+1<CR>== "Alt+j to move lines (or visual block) down"
-nnoremap <A-k> :m .-2<CR>== "Alt+k to move lines (or visual block) up"
+"Alt+j to move lines (or visual block) down"
+nnoremap <A-j> :m .+1<CR>== 
+
+"Alt+k to move lines (or visual block) up"
+nnoremap <A-k> :m .-2<CR>== 
 inoremap <A-j> <Esc>:m .+1<CR>==gi
 inoremap <A-k> <Esc>:m .-2<CR>==gi
 vnoremap <A-j> :m '>+1<CR>gv=gv
@@ -62,13 +69,20 @@ nnoremap <silent><C-j> :set paste<CR>m\`o<Esc>\`\`:set nopaste<CR>
 nnoremap <silent><C-k> :set paste<CR>m\`O<Esc>\`\`:set nopaste<CR> 
 "Ctrl-k inserts a blank line above the current line."
 
-map <C-o> :NERDTreeToggle<CR> "Map Ctrl+o to toggle NERDTree"
+"Map Ctrl+o to toggle NERDTree"
+map <C-o> :NERDTreeToggle<CR> 
 
-"let g:gitgutter_enabled = 0" "Disable GitGutter on Default"
-map <C-g> :GitGutterToggle <CR> "Ctrl+g to toggle GitGutter"
+"Disable GitGutter on Default"
+"let g:gitgutter_enabled = 0" 
 
-let g:ale_enabled = 0 "Disable Ale on default"
-map <C-a> :ALEToggle <CR> "Ctrl+a to toggle ALE"
+"Ctrl+g to toggle GitGutter"
+map <C-g> :GitGutterToggle <CR> 
+
+"Disable Ale on default"
+let g:ale_enabled = 0 
+
+"Ctrl+a to toggle ALE"
+map <C-a> :ALEToggle <CR> 
 
 "; to toggle fzf"
 map ; :Files<CR> 
